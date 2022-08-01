379bd90b8953d91ffb55cbb797755178952566a7
aaronmfuller <aaronmfuller@gmail.com>
2022-08-01 00:00:17 -0600

    Added social media icons. Still working out how to make the colors right for them

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import footStyle from './footStyle.module.css'
 
+
 const Footer = () => {
   return (
     <footer className={footStyle.container}>
       <div className={footStyle.legalContent}>
             Copyright &copy; Proshop
       </div>
-      <div clasName={ footStyle.socialIcons}>
-        <h1>Hello world</h1>
+      <div className={footStyle.socialIcons}>
+        <ul class="socialIcons">
+          <li class="facebook"><a href="https://www.facebook.com/aaronmfuller"><i class="fa fa-fw fa-facebook"></i>Facebook</a></li>
+          <li class="twitter"><a href="https://www.twitter.com"><i class="fa fa-fw fa-twitter"></i>Twitter</a></li>
+          <li class="instagram"><a href="https://www.instagram.com/aaronfuller_lv"><i class="fa fa-fw fa-instagram"></i>Instagram</a></li>
+          <li class="steam"><a href="https://www.linkedin.com/in/aaron-fuller-01809b12/"><i class="fa fa-fw fa-linkedin"></i>Steam</a></li>
+        </ul>
       </div>
 
     </footer>
