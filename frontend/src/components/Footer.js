6f06cb7443cd36c12d91b28604197f084f97a05e
Cesar Gomez <zootechdrum@gmail.com>
2022-08-11 21:49:48 -0700

    fix footer style

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,11 +9,11 @@ const Footer = () => {
             Copyright &copy; Proshop
       </div>
       <div className={footStyle.socialIcons}>
-        <ul class="socialIcons">
-          <li class="facebook"><a href="https://www.facebook.com/aaronmfuller"><i class="fa fa-fw fa-facebook"></i>Facebook</a></li>
-          <li class="twitter"><a href="https://www.twitter.com"><i class="fa fa-fw fa-twitter"></i>Twitter</a></li>
-          <li class="instagram"><a href="https://www.instagram.com/aaronfuller_lv"><i class="fa fa-fw fa-instagram"></i>Instagram</a></li>
-          <li class="steam"><a href="https://www.linkedin.com/in/aaron-fuller-01809b12/"><i class="fa fa-fw fa-linkedin"></i>LinkedIn</a></li>
+        <ul class={footStyle.socialIcons}>
+          <li class={footStyle.facebook}><a href="https://www.facebook.com/aaronmfuller"><i class="fa fa-fw fa-facebook"></i>Facebook</a></li>
+          <li class={footStyle.twitter}><a href="https://www.twitter.com"><i class="fa fa-fw fa-twitter"></i>Twitter</a></li>
+          <li class={footStyle.instagram}><a href="https://www.instagram.com/aaronfuller_lv"><i class="fa fa-fw fa-instagram"></i>Instagram</a></li>
+          <li class={footStyle.linkedin}><a href="https://www.linkedin.com/in/aaron-fuller-01809b12/"><i class="fa fa-fw fa-linkedin"></i>LinkedIn</a></li>
         </ul>
       </div>
 
